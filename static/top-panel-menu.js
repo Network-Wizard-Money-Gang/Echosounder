dcfac1956c258f95d6ab9296dccf4f7edec5b5bf
alexcombessie <patrick.baus@physik.tu-darmstadt.de>
2025-09-06 17:36:53 +0200

    fix bug graphique menu non effacé en fond

--- static/top-panel-menu.js
+++ static/top-panel-menu.js
@@ -108,6 +108,9 @@ export default Vue.createApp({
       // fonction de reset du panel : 
       resetPanel : function() {
         this.menuConf = false;
+        this.menuConfState = false; 
+        this.menuConfNetwork = false; 
+        this.menuConfTheme = false
       }
   },
 
